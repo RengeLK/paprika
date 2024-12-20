54ceb1848421990f3db38c71c2b15c108874b6ad
RengeLK <lukajda.konecny@outlook.cz>
2024-12-20 17:19:49 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,146 @@
+##########################
+## cool paprika program ##
+## main.py - req parts  ##
+## -renge 2024          ##
+##########################
+from flask import Flask, render_template, send_from_directory, make_response
+from datetime import datetime
+import requests
+import feedparser
+from secret import owmkey, jarlist
+app = Flask(__name__)
+
+
+# Format data from RSS feeds for display
+def fetch_rss_feed(url, cnt):
+    feed = feedparser.parse(url)
+    articles = []
+    marisa = cnt
+
+    for entry in feed.entries:
+        if marisa == 0:
+            break
+        articles.append({
+            "title": entry.title,
+            "link": entry.link,
+            "summary": entry.summary if "summary" in entry else "",
+            "published": entry.published if "published" in entry else "an unknown date"
+        })
+        marisa -= 1
+    return articles
+
+# Format the response correctly
+def render_xhtml(template_name, **context):
+    resp = make_response(render_template(template_name, **context))
+    resp.headers['X-Bara'] = 'pica'
+    resp.content_type = 'application/xhtml+xml'
+    return resp
+
+
+# Serve the home page
+@app.route("/")
+def home():
+    return render_xhtml("home.xhtml", title="Home", last_update='20/12/24')
+
+# Serve the downloads section
+@app.route("/downloads")
+def downloads():
+    return render_xhtml("dlindex.xhtml", title="Downloads")
+
+@app.route("/dls/java")
+def dljars():
+    return render_xhtml("dljars.xhtml", title="Java ME DLs", list=jarlist)
+
+# Serve the weather section
+@app.route("/weather")
+def weather():
+    # TODO: Actual location grabbing or something idk
+    lat = str(50.0874654)
+    lon = str(14.4212535)
+    loc = 'Gensokyo'
+
+    get = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={owmkey}&units=metric")
+    air = requests.get(f"https://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={owmkey}")
+    dat = get.json()
+    ford = air.json()['list']
+
+    weather_info = {
+        'main': dat['weather'][0]['main'],
+        'condition': dat['weather'][0]['description'],
+        'icon': dat['weather'][0]['icon'],
+        'temp': str(dat['main']['temp']) + '°C',
+        'feels': str(dat['main']['feels_like']) + '°C',
+        'pressure': str(dat['main']['pressure']) + ' hPa',
+        'humidity': str(dat['main']['humidity']) + '%',
+        'visibility': str(dat['visibility']) + 'm',
+        'wind': str(dat['wind']['speed']) + ' m/s',
+        'direct': str(dat['wind']['deg']) + '°',
+        'clouds': str(dat['clouds']['all']) + '%',
+        'aqi': air.json()['list'][0]['main']['aqi'],
+        'reqdate': datetime.fromtimestamp(dat['dt']).strftime('%H:%M:%S'),  # unix timestamp of data calculation
+        'sunrise': datetime.fromtimestamp(dat['sys']['sunrise']).strftime('%H:%M'),
+        'sunset': datetime.fromtimestamp(dat['sys']['sunset']).strftime('%H:%M')
+    }
+
+    return render_template("weather.xhtml", title="Weather", weather=weather_info, location=loc)
+
+@app.route("/forecast")
+def forecast():
+    # TODO: Actual location grabbing or something idk
+    lat = str(50.0874654)
+    lon = str(14.4212535)
+    loc = 'Gensokyo'
+    ctime = datetime.now().strftime('%H:%M:%S')
+    cnt = 8  # amount of forecast data in timestamps (3-hour difference, 8 equals 24hrs of data)
+
+    get = requests.get(f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={owmkey}&units=metric&cnt={cnt}")
+    dat = get.json()
+    weather_info = []
+
+    for i in dat['list']:
+        weather_info.append({
+            'main': i['weather'][0]['main'],
+            'condition': i['weather'][0]['description'],
+            'icon': i['weather'][0]['icon'],
+            'temp': str(i['main']['temp']) + '°C',
+            'feels': str(i['main']['feels_like']) + '°C',
+            'pressure': str(i['main']['pressure']) + ' hPa',
+            'humidity': str(i['main']['humidity']) + '%',
+            'wind': str(i['wind']['speed']) + ' m/s',
+            'direct': str(i['wind']['deg']) + '°',
+            'clouds': str(i['clouds']['all']) + '%',
+            'prop': str(int(i['pop'] * 100)) + '%',
+            'foretime': datetime.fromtimestamp(i['dt']).strftime('%H:%M')  # forecast data time
+        })
+
+    return render_template("forecast.xhtml", title="Forecast", weather=weather_info, location=loc, time=ctime)
+
+# Serve the news section
+@app.route("/news")
+def news():
+    ctime = datetime.now().strftime('%H:%M:%S')
+    respecttheaya = 'https://bunbunmaru.news/rss'
+    rssfeed = 'https://hnrss.org/newest?q=touhou'
+    cnt = 10
+
+    artlist = fetch_rss_feed(rssfeed, cnt)
+    return render_template("news.xhtml", title="News", articles=artlist, time=ctime)
+
+# Serve the xDOS (public transport) section (NOT IMPLEMENTED)
+@app.route("/xdos")
+def xdos():
+    return 'not implemented yet'
+
+# Serve the xInfo (account) section (NOT IMPLEMENTED)
+@app.route("/xinfo")
+def xinfo():
+    return render_template("acc.xhtml")
+
+# Serve static content
+@app.route("/static/<path:filename>")
+def static_files(filename):
+    return send_from_directory("static", filename)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=4040)
---
+++ secret.py
@@ -0,0 +1,6 @@
+owmkey = "92efedec851beb32e728025bd94b3602"
+jarlist = [
+    {'name': 'Sokoban', 'uri': 'sokoban.jar'},
+    {'name': 'jSudoku', 'uri': 'jsudoku.jar'},
+    {'name': 'Yet Another Tetris', 'uri': 'tetris.jar'}
+]
